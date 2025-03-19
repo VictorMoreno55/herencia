2c17e43e91afafaac1958864ce5f576708dcd1c1
Lab2-05 <Lab2-05@LAB2-05>
2025-03-18 18:20:57 -0600

    Clases añadidas

---
+++ herencia/Alumno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace herencia
+{
+    internal class Alumno
+    {
+        public Alumno() { }
+
+        public Alumno(int matricula, string carrera)
+        {
+            Matricula = matricula;
+            Carrera = carrera;
+        }
+
+        public int Matricula { get; set; }
+        public string Carrera { get; set; }
+    }
+}
